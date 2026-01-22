0f5c968e105ecb691f56b064a6489e2c08cfcfbc
Виталий <saifutdinov_vitalii@mail.ru>
2026-01-22 22:04:30 +0500

    hgg

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -9,4 +9,17 @@
         nextEl: '.slider__btn-next',
         prevEl: '.slider__btn-prev ',
     },
+});
+
+new Swiper ('.news__swiper', {
+    direction: 'horizontal',
+    loop: true,
+    slidesPerView: 4,
+    pagination: {
+        el: '.swiper-pagination',
+    },
+    navigation: {
+        nextEl: '.news__btn-next',
+        prevEl: '.slider__btn-prev ',
+    },
 }) 
