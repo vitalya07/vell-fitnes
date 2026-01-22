35d9d05d8b62f45c549e06b5295bb66f51249020
Виталий <saifutdinov_vitalii@mail.ru>
2026-01-22 15:37:12 +0500

    ghjh

---
+++ assets/js/slider.js
@@ -0,0 +1,12 @@
+ new Swiper ('.special__swiper', {
+    direction: 'horizontal',
+    loop: true,
+    slidesPerView: 4,
+    pagination: {
+        el: '.swiper-pagination',
+    },
+    navigation: {
+        nextEl: '.slider__btn-next',
+        prevEl: '.slider__btn-prev ',
+    },
+}) 
