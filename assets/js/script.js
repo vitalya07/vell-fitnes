f59ee9c207ed6955e00d2c03a41790bb790ef7db
Vitalyi <saifutdinov_vitalii@mail.ru>
2026-01-24 00:09:52 +0500

    start table version

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,24 @@
-const headerWrapper = document.querySelector('.header');
+const header = document.querySelector('.header');
+const block = document.querySelector('.block');
+let offsetBlock = header.offsetHeight;
 
 window.addEventListener('scroll', ()=> {
-    if(window.scrollY > 1200) {
-        headerWrapper.style.position = 'fixed';
-    } else {
-        headerWrapper.style.position = 'static';
+    if(window.scrollY > 400) {
+        block.style.display = 'block';
+        block.style.height = offsetBlock + 'px';
+        header.style.position = 'fixed';
+        header.style.top = `-100%`;
+        header.style.left = `0`;       
+    }
+    if(window.scrollY > 1000) {
+        header.style.position = 'fixed';
+        header.style.top = `0`;
+        header.style.left = '0';}
+    // } else {
+    //     headerWrapper.style.position = 'static';
+    // }
+    if(window.scrollY < 800) {
+        block.style.display = 'none';
+       header.style.position = 'static';        
     }
 })
