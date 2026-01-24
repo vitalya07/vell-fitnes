66a5136cd11385fdcf6f292192876ed59a57beec
Vitalyi <saifutdinov_vitalii@mail.ru>
2026-01-24 23:21:17 +0500

    gfggh

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -1,14 +1,15 @@
  new Swiper ('.special__swiper', {
     direction: 'horizontal',
-    loop: true,
-    slidesPerView: 4,
-    pagination: {
-        el: '.swiper-pagination',
-    },
+    loop: true,    
     navigation: {
         nextEl: '.slider__btn-next',
         prevEl: '.slider__btn-prev ',
     },
+    breakpoints: {
+        320: { slidesPerView: 1 }, // Для мобильных
+        768: { slidesPerView: 3,  spaceBetween: -60 }, // Для планшетов
+        1024: { slidesPerView: 4,  spaceBetween: 30 } // Для десктопов
+    }
 });
 
 new Swiper ('.news__swiper', {
